65d10613ea84d4b3c9b1bd04afabe23eccfdc667
Kaelyn Hughes <kaelynmhughes@gmail.com>
2024-06-27 12:06:20 -0600

    add colorizer, continue updating theme

--- nvim/lua/core/opt.lua
+++ nvim/lua/core/opt.lua
@@ -10,7 +10,63 @@ vim.opt.shiftwidth = 4
 -- true color support
 vim.opt.termguicolors = true
 
--- color scheme
+-- set theme options
+require("catppuccin").setup({
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    transparent_background = false, -- disables setting the background color.
+    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+    dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+    },
+    no_italic = false, -- Force no italic
+    no_bold = false, -- Force no bold
+    no_underline = false, -- Force no underline
+    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" }, -- Change the style of comments
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
+    },
+    color_overrides = {
+        mocha = {
+            base = '#320044',
+            mantle = '#000000',
+            crust = '#000000',
+            text = '#EFCBFB',
+            peach = '#F09867',
+            blue = '#c1fcf5',
+            lavender = '#E9A6FF'
+        },
+    },
+    custom_highlights = {},
+    default_integrations = true,
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+            enabled = true,
+            indentscope_color = "",
+        },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
+})
+
+-- setup must be called before loading
 vim.cmd.colorscheme "catppuccin"
 
 -- disable mouse :P
