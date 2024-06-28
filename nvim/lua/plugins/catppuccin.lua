eaa804cb6ea4b5a1261566494a67a706221a186b
Kaelyn Hughes <119621093+kaelynhughes@users.noreply.github.com>
2024-06-28 16:59:35 -0600

    remove useless code from catpuccin plugin page

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -2,11 +2,4 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    config = function()
-        require("catppuccin").setup({
-            color_overrides = {
-                base = ""     
-            }
-        })
-    end,
 }
