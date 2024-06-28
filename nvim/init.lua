86be187f305ff05541bcfdbac8473d375f05322d
Kaelyn Hughes <119621093+kaelynhughes@users.noreply.github.com>
2024-06-28 16:56:40 -0600

    add purple gradient for text, update alacritty theme, fix a bug related to colorizer

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = ' '
+vim.opt.termguicolors = true
 require('core/lazy')
 require('lazy').setup('plugins', opts)
 require('core/keymaps')
--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,5 +1,12 @@
 return {
     "catppuccin/nvim",
     name = "catppuccin",
-    priority = 1000
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            color_overrides = {
+                base = ""     
+            }
+        })
+    end,
 }
