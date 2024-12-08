e232a00ab36ca1076e17599cd331c330e3957e0e
kaelynhughes <kaelynmhughes@gmail.com>
2024-12-07 18:30:03 -0700

    linux stuff

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,9 +1,9 @@
 return {
-  'nvim-telescope/telescope.nvim', 
+  'nvim-telescope/telescope.nvim',
 	tag = '0.1.6',
 	dependencies = {
         'nvim-lua/plenary.nvim',
-        { 'nvim-telescope/telescope-fzf-native.nvim', build='make' },
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
     },
     config = function()
         require('telescope').load_extension('fzf')
