3c3c717b3f5dba68c6fdaa90125edbec8621a1b3
Kaelyn Hughes <119621093+kaelynhughes@users.noreply.github.com>
2025-02-10 11:57:11 -0700

    fix lualine & add render-markdown; luarocks added for compatibility

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -30,14 +30,7 @@ return {
                 lualine_y = {'progress'},
                 lualine_z = {'location'}
               },
-              inactive_sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = {'filename'},
-                lualine_x = {'location'},
-                lualine_y = {},
-                lualine_z = {}
-              },
+              inactive_sections = {},
               tabline = {},
               winbar = {},
               inactive_winbar = {},
