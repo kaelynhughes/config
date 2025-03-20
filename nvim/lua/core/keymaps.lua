a008b630801a629fb696ffcd786b327427294371
Kaelyn Hughes <119621093+kaelynhughes@users.noreply.github.com>
2025-03-20 16:21:59 -0600

    add git blame plugin, begin updating inline diagnostic symbols to match lualine

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,27 +1,36 @@
--- telescope keymaps --
+-- import apis --
 local telescope = require('telescope.builtin')
 local tree = require('nvim-tree.api')
 local md = require('render-markdown')
+local blame = require('gitblame')
+
+-- telescope keymaps --
 vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
 vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
 vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
 vim.keymap.set('n', '<leader>fh', telescope.help_tags, {})
 
+-- nvim-tree keymaps --
 vim.keymap.set('n', '<leader>t', tree.tree.open, {})
 vim.keymap.set('n', '<leader>c', tree.tree.close, {})
 
+-- markdown rendering --
 vim.keymap.set('n', '<leader>md', md.toggle, {})
 
--- Turn off inline diagnostics & add keymaps to see them in a floating window
+-- diagnostic display --
 vim.diagnostic.config({
     virtual_text = false,
     signs = {
         text = {
-            [vim.diagnostic.severity.ERROR] = '',
+            [vim.diagnostic.severity.ERROR] = '󰅚',
             [vim.diagnostic.severity.WARN] = '',
+            [vim.diagnostic.severity.HINT] = '󰌶'
         }
     }
 });
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, {});
 vim.keymap.set('n', '<leader>n', vim.diagnostic.goto_next, {});
-vim.keymap.set('n', '<leader>b', vim.diagnostic.goto_prev, {});
+vim.keymap.set('n', '<leader>p', vim.diagnostic.goto_prev, {});
+
+-- git blame --
+vim.keymap.set('n', '<leader>b', blame.toggle, {})
