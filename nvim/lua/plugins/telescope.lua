4393766fdfa19b8db473fb387db52d27348bdabb
Kaelyn Hughes <119621093+kaelynhughes@users.noreply.github.com>
2025-03-27 12:39:50 -0600

    change telescope close keymap to ctrl-c in normal mode. also set telescope to automaticaly update i think

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,11 +1,20 @@
 return {
   'nvim-telescope/telescope.nvim', 
-	tag = '0.1.6',
+	tag = '0.1.x',
 	dependencies = {
         'nvim-lua/plenary.nvim',
         { 'nvim-telescope/telescope-fzf-native.nvim', build='make' },
     },
     config = function()
         require('telescope').load_extension('fzf')
+        require('telescope').setup {
+          defaults = {
+            mappings = {
+              n = {
+                ["<C-c>"] = "close"
+              }
+            }
+          }
+        }
     end,
 }
